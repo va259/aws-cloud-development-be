839030dc0ed781c6b7b8d71c2715225a6dabec6f
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-16 17:49:11 +0200

    init: sws service created & deployed

---
+++ products-service/assets/fixtures.js
@@ -0,0 +1,74 @@
+module.exports.products = [
+  {
+    "count": 4,
+    "description": "Generates two continuously varying random voltages, two pulse actuated unpredictable voltages, and audio noise with three spectral distributions. ",
+    "id": "bfd21daa-ec93-11ec-8ea0-0242ac120002",
+    "price": 600,
+    "title": "Source of Uncertainty Model 266r Rev 3.0",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/266.jpg"
+  },
+  {
+    "count": 6,
+    "description": "Two independent voltage controlled oscillators, continuously tunable from 5Hz to 20kHz (applied control voltages can extend limits to .1Hz and 30kHz).",
+    "id": "bfd22282-ec93-11ec-8ea0-0242ac120002",
+    "price": 700,
+    "title": "Dual Oscillator Model 258c",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/291.jpg"
+  },
+  {
+    "count": 5,
+    "description": "It comprises four function generators organized in two pairs. All four generators can operate independently or can be linked in pairs to generate more complex output voltages",
+    "id": "bfd223c2-ec93-11ec-8ea0-0242ac120002",
+    "price": 650,
+    "title": "Quad Function Generator Model 281r",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/281.jpg"
+  },
+  {
+    "count": 4,
+    "description": "Four independent gates with selectable amplitude-dependents spectral characteristics.",
+    "id": "bfd224e4-ec93-11ec-8ea0-0242ac120002",
+    "price": 700,
+    "title": "Quad Lowpass Gate Model 292c Rev 3.0",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/292.jpg"
+  },
+  {
+    "count": 4,
+    "description": "Mixer/Preamplifier Model 207 includes a six channel stereo mixer with voltage-controlled panning and a single channel preamplifier with impedance and gain switches for a variety of input levels",
+    "id": "bfd22610-ec93-11ec-8ea0-0242ac120002",
+    "price": 600,
+    "title": "Mixer/Preamplifier Model 207r",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/207.jpg"
+  },
+  {
+    "count": 2,
+    "description": "Four independent sample-and-hold circuits, plus a specialized logic circuit for expediently implementing polyphonic patches.",
+    "id": "bfd2270a-ec93-11ec-8ea0-0242ac120002",
+    "price": 650,
+    "title": "Quad Sample And Hold Model 264m",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/264.jpg"
+  },
+  {
+    "count": 3,
+    "description": "Two bandpass filters with voltage-controlled center frequencies and bandwidths.",
+    "id": "bfd22822-ec93-11ec-8ea0-0242ac120002",
+    "price": 700,
+    "title": "Dual Voltage Controlled Filter Model 291r",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/291.jpg"
+  },
+  {
+    "count": 2,
+    "description": "Four independent sample-and-hold circuits, plus a specialized logic circuit for expediently implementing polyphonic patches.",
+    "id": "bfd22b60-ec93-11ec-8ea0-0242ac120002",
+    "price": 700,
+    "title": "Analogue Waveform Generator Model 25s",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/25s.jpg"
+  },
+  {
+    "count": 4,
+    "description": "The Model 259 Programmable Complex Waveform Generator introduces several innovative techniques for dealing with complex timbres.",
+    "id": "bfd22cdc-ec93-11ec-8ea0-0242ac120002",
+    "price": 950,
+    "title": "Programmable Complex Waveform Generator Model 259r",
+    "image": "https://rss-react-shop-assets.s3.amazonaws.com/259.jpg"
+  }
+]
